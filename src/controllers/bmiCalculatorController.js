9a42f4b06a842a30464b52bcae57e5e0c32be9cd
Simran <simran.kahlon@graymatrix.com>
2021-04-27 17:32:20 +0530

    updated the calculate BMI method

--- src/controllers/bmiCalculatorController.js
+++ src/controllers/bmiCalculatorController.js
@@ -33,7 +33,7 @@ module.exports.BMICalculatorController = class BMICalculatorController {
             var jsonData = await bmiObj.parseFile(fileName);
             //Write the output to a file.
             var outputFilePath = await readWriteObj.writeOutputToFile(jsonData,file);
-            //Flag to check whether the output file has to be uploaded on s3
+            //Flag to check whether the output file has to be uploaded on s3.
             if(uploadToS3)
             {
                 var s3URL = await readWriteObj.writeToS3(outputFilePath,file);
@@ -59,5 +59,4 @@ module.exports.BMICalculatorController = class BMICalculatorController {
             });
         }
     }
-
 };
